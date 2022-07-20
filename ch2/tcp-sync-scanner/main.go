854890f4df3f50540fc9549041fa6209d42030f1
J3ssh <87490958+J3ssh@users.noreply.github.com>
2022-07-20 09:19:14 +0100

    Update main.go

--- ch2/tcp-sync-scanner/main.go
+++ ch2/tcp-sync-scanner/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func worker(ports chan int, wg *sync.WaitGroup) {
-	for p := range ports {
+	for p := range ports { //rececive from the port channel, looping till closed
 		fmt.Println(p)
 		wg.Done()
 	}
@@ -15,12 +15,12 @@ func worker(ports chan int, wg *sync.WaitGroup) {
 func main() {
 	ports := make(chan int, 100) //creating channel, channel limit cap = 100, can only hold 100 itmes
 	var wg sync.WaitGroup
-	for i := 0; i < cap(ports); i++ { //for loop to create number of worker goroutines
+	for i := 0; i < cap(ports); i++ { //for loop to create number of workers
 		go worker(ports, &wg)
 	}
 	for i := 1; i <= 1024; i++ { //for loop port generation
 		wg.Add(1)
-		ports <- i
+		ports <- i //sending ports to port channel 
 	}
 	wg.Wait()
 	close(ports) //closing channel
